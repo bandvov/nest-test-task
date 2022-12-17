ecc06f8887bdce0c1293e52b87116313ae207cc0
Volodymyr Vasylkiv <bandvov@gmail.com>
2022-12-17 16:45:37 +0200

    add front

---
+++ front/src/App.tsx
@@ -0,0 +1,28 @@
+import React, { ReactElement } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ImagesList from "./components/imagesList/ImagesList";
+import UploadForm from "./components/uploadForm/UploadForm";
+
+const routes: { path: string; element: ReactElement }[] = [
+  {
+    path: "/",
+    element: <UploadForm />,
+  },
+  { path: "/list", element: <ImagesList /> },
+];
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/test" />
+          {routes.map((route) => {
+            return <Route {...route} />;
+          })}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/components/uploadForm/UploadForm.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./UploadForm.styles.css";
+
+export default function UploadForm() {
+  const [imageUrl, setImageUrl] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
+  const [error, setError] = useState<boolean>(false);
+
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setImageUrl(e.target.value);
+  };
+  const submitHandler = (e: React.MouseEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setImageUrl("");
+    setMessage("");
+    setError(false);
+    axios
+      .post("http://localhost:3001/image", {
+        imageUrl,
+        token: process.env.REACT_APP_TOKEN,
+      })
+      .then((res) => {
+        setMessage(res.data);
+        return res;
+      })
+      .catch((e) => {
+        setMessage(e.message);
+        setError(true);
+      });
+  };
+  return (
+    <form className="form" onSubmit={submitHandler}>
+      <h3>Відправити фото</h3>
+      <input onChange={changeHandler} value={imageUrl} />
+      <div className="error-wrapper">
+        {error && <span className={error ? "error" : ""}>{message}</span>}
+      </div>
+      <button type="submit">Завантажити</button>
+    </form>
+  );
+}
