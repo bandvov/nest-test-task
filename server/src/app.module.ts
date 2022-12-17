a4772fdabc308518f7ce14063ff52e883522b553
Volodymyr Vasylkiv <bandvov@gmail.com>
2022-12-17 18:11:10 +0200

    add env support

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ImageModule } from './image/image.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), ImageModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    ImageModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
