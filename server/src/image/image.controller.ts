4e1430e66352e389e19bf798d7d772bf42afbe0e
Volodymyr Vasylkiv <bandvov@gmail.com>
2022-12-18 09:57:14 +0200

    add file upload

--- server/src/image/image.controller.ts
+++ server/src/image/image.controller.ts
@@ -4,11 +4,15 @@ import { ImageService } from './image.service';
 import {
   Body,
   Controller,
+  InternalServerErrorException,
   Post,
   UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
+import * as fs from 'fs';
+import axios from 'axios';
+import * as path from 'path';
 
 @Controller('image')
 export class ImageController {
@@ -16,7 +20,30 @@ export class ImageController {
   @Post()
   @UseGuards(TokenGuard)
   @UsePipes(ValidationPipe)
-  test(@Body() imageUrl: UploadImageDto) {
-    return this.imageService.sendHello(imageUrl);
+  async test(@Body() body: UploadImageDto) {
+    const fileName = body.imageUrl.match(/[\w-]+.(jpg|png|svg)/g)[0];
+    const writer = fs.createWriteStream(
+      path.join(process.cwd(), 'uploads', fileName),
+    );
+
+    const streamResponse = await axios({
+      url: body.imageUrl,
+      method: 'GET',
+      responseType: 'stream',
+    });
+
+    streamResponse.data.pipe(writer);
+    return new Promise((resolve, reject) => {
+      writer.on('finish', () => {
+        console.log('Finished');
+        resolve('Image uploaded');
+      });
+      writer.on('error', () => {
+        console.log('Finished');
+        reject(new InternalServerErrorException());
+      });
+    });
+
+    // return this.imageService.sendHello(imageUrl);
   }
 }
